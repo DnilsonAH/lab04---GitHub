d2cd1074914b72613c637a354e46c3380064001c
Dnilson Achahuanco Huarilloclla <dnilson.achahuanco@tecsup.edu.pe>
2025-04-10 14:03:32 -0500

    Revert "feat: Add ViewHolaCurso"
    This reverts commit f824a5485821cd9709696ce7dc5a6e7881af5dfc.

--- app/src/main/java/com/tecsup/dnilsontec04/MainActivity.kt
+++ app/src/main/java/com/tecsup/dnilsontec04/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -21,7 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DnilsonTec04Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -33,31 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
-
 
 @Preview(showBackground = true)
 @Composable
-fun ViewHolaCursoPreview() {
+fun GreetingPreview() {
     DnilsonTec04Theme {
-        ViewHolaCurso()
+        Greeting("Android")
     }
 }
